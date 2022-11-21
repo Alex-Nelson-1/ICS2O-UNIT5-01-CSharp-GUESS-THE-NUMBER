421dc2a3e9ce6c21517c62bc086d7685e10b838c
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-11-20 18:26:02 -0800

    Update main.cs

--- main.cs
+++ main.cs
@@ -12,18 +12,18 @@ class Program
         int guessValue;
         //Returns an `int` value greater in the range 1 <= value <7
         Random rd = new Random();
-        int randomNumber = rd.Next(1,7);
+        int randomNumber = rd.Next(1, 7);
         Console.WriteLine("This program allows you to guess a hidden number");
         Console.WriteLine("");
         Console.WriteLine("Guess a number between 1 and 6: ");
         guessValue = Convert.ToInt32(Console.ReadLine());
         //Output
         Console.WriteLine("");
-        if(guessValue == randomNumber)
+        if (guessValue == randomNumber)
         {
-            Console.WriteLine("Correct! " + randomNumber + " was the right number!" );
+            Console.WriteLine("Correct! " + randomNumber + " was the right number!");
         }
-        if(guessValue != randomNumber)
+        if (guessValue != randomNumber)
         {
             Console.WriteLine("incorrect! The correct number was " + randomNumber + ". Restart and try again!");
         }
